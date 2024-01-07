81403bca275d8ec83569cf4ff07df531136406f1
piyushimraw <piyush.srivastavaimraw@gmail.com>
2024-01-07 22:33:07 +0530

    fixed styles for buttos

--- src/module/seats-view/confirm-booking.tsx
+++ src/module/seats-view/confirm-booking.tsx
@@ -46,7 +46,7 @@ const ConfirmSeatBooking = ({
         <div className="flex justify-end gap-4">
           <button
             type="button"
-            className="bg-gray-300 text-gray-700 hover:bg-gray-400 px-4 py-2 rounded-md"
+            className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
             onClick={() => {
               onCancel();
             }}
@@ -55,7 +55,7 @@ const ConfirmSeatBooking = ({
           </button>
           <button
             type="submit"
-            className="bg-blue-500 text-white hover:bg-blue-600 px-4 py-2 rounded-md"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             onClick={() => {
               form.handleSubmit((data) => {onSubmit(data)})();
             }}
