f61344fda3b3d6d836f77c2a290d92a8fd0dac79
piyushimraw <piyush.srivastavaimraw@gmail.com>
2024-01-07 16:03:38 +0530

    added initial setup files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1 className="text-3xl font-bold underline text-blue-950">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
